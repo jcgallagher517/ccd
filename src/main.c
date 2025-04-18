485de0b78211b53c05bef152c54723c6f373612e
Johnny <jcgall517@gmail.com>
2025-04-17 20:46:34 -0600

    fixed silly bug, still avoiding actual logic bit

--- src/main.c
+++ src/main.c
@@ -8,26 +8,32 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+#define COLS 256
+#define LLEN ((2*(int)sizeof(unsigned long)) + 4 + (9*COLS-1) + COLS + 2)
 
 int main(int argc, char* argv[]) {
 
-  FILE *input = NULL;
-  int opt;
-
-  // default parameter values
+  /* default parameter values */
+  char help_link[] = "https://linux.die.net/man/1/xxd";
   bool autoskip = false;
   bool bits = false;
   int cols = 16;
   int groupsize = 2;
-  bool help = false;
   bool include = false;
-  int len = 0; // what to put for default value here?
+  int len = 0; /* 0 means read til EOF */
   bool plain = false;
-  int seek;
+  int seek = 0; /* initial offset in bytes */
   bool uppercase = false;
 
+  /* parse cli args */
+  /* atoi can be unsafe for invalid inputs, consider using strtol instead */
+  int opt;
   while ((opt = getopt(argc, argv, "abc:g:hil:ps:u")) != -1) {
     switch (opt) {
+    case 'h':
+      printf("Usage: %s [options] [infile]\n", argv[0]);
+      printf("Please visit %s for full documentation.\n", help_link);
+      return EXIT_SUCCESS;
     case 'a':
       autoskip = true;
       break;
@@ -36,15 +42,14 @@ int main(int argc, char* argv[]) {
       break;
     case 'c':
       cols = atoi(optarg);
-      if (cols > 256) {
-        fprintf(stderr, "%s: invalid number of columns (max: 256)\n", argv[0]);
+      if (cols > COLS) {
+        fprintf(stderr, "%s: invalid number of columns (max: %d)\n", argv[0], COLS);
         return EXIT_FAILURE;
       }
+      break;
     case 'g':
       groupsize = atoi(optarg);
-      break;
-    case 'h':
-      help = true;
+      groupsize = (groupsize >= 0) ? groupsize : 2;
       break;
     case 'i':
       include = true;
@@ -66,36 +71,21 @@ int main(int argc, char* argv[]) {
     }
   }
 
+  /*
   // to test that arg parsing went well, print everything here
   printf("autoskip: %d\n", autoskip);
   printf("bits: %d\n", bits);
   printf("cols: %d\n", cols);
   printf("groupsize: %d\n", groupsize);
-  printf("help: %d\n", help);
   printf("include: %d\n", include);
   printf("len: %d\n", len);
   printf("plain: %d\n", plain);
   printf("seek: %d\n", seek);
   printf("uppercase: %d\n", uppercase);
+  */
 
-  /* this revealed a peculiar bug:
-     if I specify columns, it also changes groupsize
-
-     e.g.
-     ccd -c 10
-     =>
-     cols = 10
-     groupsize = 10
-
-     ccd -c 10 -g 20
-     cols = 10
-     groupsize = 20
-     
-     fix this later
-   */
-
-
-  // open file if available, else use stdin
+  /* open file if available, else use stdin */
+  FILE *input = NULL;
   if (optind < argc) {
     input = fopen(argv[optind], "r");
     if (input == NULL) {
@@ -107,10 +97,14 @@ int main(int argc, char* argv[]) {
   }
 
 
-  // start actual processing here
-  char buffer[1024];
+  /* start actual processing here */
+  char buffer[LLEN];
+  printf("%d\n", LLEN);
   // while (fgets(buffer, sizeof(buffer), input)) {
   // }
+
+
+
 
   if (input != stdin) {
     fclose(input);
